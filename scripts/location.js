55b9afb7d0e992ff7bb941ed4be5e8f09eb172f7
CodedGlory <aisha.busari@gmail.com>
2023-02-01 03:13:27 -0500

    Add comment labeling location hover

--- scripts/location.js
+++ scripts/location.js
@@ -1,14 +1,18 @@
 const locationBar = document.getElementById('location-bar');
 const locationArrow = document.getElementById('location-arrow');
 
-/*Create hover color swap on location bar - on mouuseenter*/
+/*LOCATION HOVER*/
+/*Create color swap on hover of location bar - on mouuseenter*/
 locationBar.onmouseover = function(){
     locationBar.style.backgroundColor = "#d5966c";
     locationArrow.style.backgroundColor = "#151515";
 }
 
-/*Remove hover color swap - on mouseleave*/
+/*Remove color swap hover - on mouseleave */
 locationBar.onmouseleave = function(){
     locationBar.style.backgroundColor = "#151515";
     locationArrow.style.backgroundColor = "#d5966c";
 }
+
+
+
