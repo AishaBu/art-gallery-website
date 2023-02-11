11c8e4a1f64ef88b4a31629f55b8cce8850d9870
CodedGlory <aisha.busari@gmail.com>
2023-02-11 13:30:47 -0500

    Put back home arrow in front of map and position left top

--- scripts/location-page.js
+++ scripts/location-page.js
@@ -1,19 +1,3 @@
-/*BACK HOME BAR HOVER*/
-const backHomeBar = document.getElementById('back-home-bar');
-const backHomeArrow = document.getElementById('back-home-arrow');
-
-/*Create color swap on hover of location bar - on mouuseenter*/
-backHomeBar.onmouseover = function(){
-    backHomeBar.style.backgroundColor = "#d5966c";
-    backHomeArrow.style.backgroundColor = "#151515";
-}
-
-/*Remove color swap hover - on mouseleave*/
-backHomeBar.onmouseleave = function(){
-   backHomeBar.style.backgroundColor = "#151515";
-    backHomeArrow.style.backgroundColor = "#d5966c";
-}
-
 /*Leaflet Map*/ /*Create Map With Coordinates*/
 const leafletMap = L.map('leaflet-map').setView([41.4815, -71.3103], 16);
 
