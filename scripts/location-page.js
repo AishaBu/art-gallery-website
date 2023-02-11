7802c8429f69de6cccaeec29d34517b3cd10194b
CodedGlory <aisha.busari@gmail.com>
2023-02-11 11:51:00 -0500

    Fix quotes in meta tag

--- scripts/location-page.js
+++ scripts/location-page.js
@@ -16,10 +16,32 @@ backHomeBar.onmouseleave = function(){
 
 /*Leaflet Map*/ 
 /*Create Map With Coordinates*/
-const leafletMap = L.map('leaflet-map').setView([51.505, -0.09], 13);
+const leafletMap = L.map('leaflet-map').setView([41.4815, -71.3103], 16);
 
 /*Add Open Street Map Tiles*/
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom:6,
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
 }).addTo(leafletMap);
+
+
+/*Map Controls*/
+/*Remove default map controls*/
+leafletMap.zoomControl.remove();
+
+/*Change position of map controls*/
+L.control.zoom({
+    position: 'bottomright'
+}).addTo(leafletMap);
+
+/*Add Custom Map Markers*/
+let mapMarker = L.icon({
+    iconUrl: '../assets/icon-location.svg',
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+/*Add marker to map*/
+L.marker([41.4815, -71.3103], {icon: mapMarker}).addTo(leafletMap);
